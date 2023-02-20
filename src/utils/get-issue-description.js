5dcdfa82728af78d97a4d6c6d8db0674e6cbeea6
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-02-20 17:04:32 -0500

    chore: include a new js file to get the issue description.

---
+++ src/utils/get-issue-description.js
@@ -0,0 +1,27 @@
+const { Octokit } = require("@octokit/rest");
+
+async function run() {
+  try {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+
+    const { data } = await octokit.issues.get({
+      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+      issue_number: process.env.INPUT_ISSUE_NUMBER,
+    });
+
+    console.log(`Descripción del issue: ${data.body}`);
+
+    const output = {
+      description: data.body,
+    };
+    console.log(JSON.stringify(output));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+run();
