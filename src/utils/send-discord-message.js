2048a8997e14394fdaf31a12ba0ec9eb41d788b2
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-02-20 17:04:32 -0500

    chore: include a new js file to get the issue description.

--- src/utils/send-discord-message.js
+++ src/utils/send-discord-message.js
@@ -1,14 +1,17 @@
 import fetch from "node-fetch";
 
 // const issueNumber = process.env.GITHUB_HEAD_REF.split("/").pop();
-const issueName = process.GITHUB_REF_NAME;
+const issueName = process.env.INPUT_ISSUE_NUMBER;
+const issueDescription = process.INPUT_ISSUE_DESCRIPTION;
 const reward = 10;
 const username = "username_de_discord";
 
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
 const data = {
   username: "GitHub Action",
-  content: `El pull request #${issueName} ha sido mergeado en Develop. Se otorgan ${reward} puntos a ${username}.`,
+  content: `El pull request #${issueName} ha sido mergeado en Develop. 
+  Descripcion: ${issueDescription}
+  Se otorgan ${reward} puntos a ${username}.`,
 };
 
 fetch(webhookUrl, {
