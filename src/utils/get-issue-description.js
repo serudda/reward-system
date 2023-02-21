2bb3a187c5b84a05819182c2f3bc053fa5b29593
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-02-20 19:51:21 -0500

    refactor: pass issue number

--- src/utils/get-issue-description.js
+++ src/utils/get-issue-description.js
@@ -1,29 +1,14 @@
 import { Octokit } from "@octokit/rest";
 
-function getValueAfterPrefix(text) {
-  const regex = /\/RS-(\w+)/;
-  const match = text.match(regex);
-  return match ? match[1] : null;
-}
-
 async function run() {
   try {
     const octokit = new Octokit({
       auth: process.env.PROJECT_TOKEN,
     });
 
-    const issue_number = getValueAfterPrefix(
-      process.env.GITHUB_HEAD_REF.split("RS-").pop()
-    );
+    const issue_number = process.env.GITHUB_HEAD_REF.split("RS-").pop();
     const owner = process.env.GITHUB_REPOSITORY.split("/")[0];
     const repo = process.env.GITHUB_REPOSITORY.split("/")[1];
-
-    console.log(
-      `Issue Code: ${process.env.GITHUB_HEAD_REF.split("RS-").pop()}`
-    );
-    console.log(`Owner: ${owner}`);
-    console.log(`Repo: ${repo}`);
-    console.log(`Issue Number: ${issue_number}`);
 
     const { data } = await octokit.request(
       `GET /repos/${owner}/${repo}/issues/${issue_number}`,
