a7d4d7f4de4c628a68b03f0fea461d023666f352
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-02-20 21:44:08 -0500

    refactor: send the clean info to be post in Discord

--- src/utils/send-discord-message.js
+++ src/utils/send-discord-message.js
@@ -1,9 +1,8 @@
 import fetch from "node-fetch";
 
-// const issueNumber = process.env.GITHUB_HEAD_REF.split("/").pop();
-const issueName = process.env.INPUT_ISSUE_NUMBER;
-const issueDescription = process.INPUT_ISSUE_DESCRIPTION;
-const reward = 10;
+const issueName = process.env.ISSUE_NUMBER;
+const issueDescription = process.ISSUE_DESCRIPTION;
+const reward = process.env.REWARD;
 const username = "username_de_discord";
 
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
