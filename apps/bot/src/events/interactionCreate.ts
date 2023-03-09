db1b77bf7ddfdd612c83d9d3d12afb3a07524a3e
Zyruks <kuzenshi@gmail.com>
2023-03-09 02:11:08 -0400

    docs: :memo: add comments to increase code clarity

--- apps/bot/src/events/interactionCreate.ts
+++ apps/bot/src/events/interactionCreate.ts
@@ -1,61 +1,63 @@
 import { Interaction } from "discord.js";
 import { BotEvent } from "../types";
 
+/*
+This is an event handler for a Discord bot that handles slash commands and autocomplete interactions.It includes a cooldown system to limit the rate at which users can execute certain commands.The code retrieves the corresponding command object and checks whether it has a cooldown or an autocomplete function, depending on the type of interaction.It then executes the command or calls the autocomplete function, respectively. */
 const event: BotEvent = {
-	name: "interactionCreate",
-	execute: (interaction: Interaction) => {
-		if (interaction.isChatInputCommand()) {
-			let command = interaction.client.slashCommands.get(
-				interaction.commandName
-			);
-			let cooldown = interaction.client.cooldowns.get(
-				`${interaction.commandName}-${interaction.user.username}`
-			);
-			if (!command) return;
-			if (command.cooldown && cooldown) {
-				if (Date.now() < cooldown) {
-					interaction.reply(
-						`You have to wait ${Math.floor(
-							Math.abs(Date.now() - cooldown) / 1000
-						)} second(s) to use this command again.`
-					);
-					setTimeout(() => interaction.deleteReply(), 5000);
-					return;
-				}
-				interaction.client.cooldowns.set(
-					`${interaction.commandName}-${interaction.user.username}`,
-					Date.now() + command.cooldown * 1000
-				);
-				setTimeout(() => {
-					interaction.client.cooldowns.delete(
-						`${interaction.commandName}-${interaction.user.username}`
-					);
-				}, command.cooldown * 1000);
-			} else if (command.cooldown && !cooldown) {
-				interaction.client.cooldowns.set(
-					`${interaction.commandName}-${interaction.user.username}`,
-					Date.now() + command.cooldown * 1000
-				);
-			}
-			command.execute(interaction);
-		} else if (interaction.isAutocomplete()) {
-			const command = interaction.client.slashCommands.get(
-				interaction.commandName
-			);
-			if (!command) {
-				console.error(
-					`No command matching ${interaction.commandName} was found.`
-				);
-				return;
-			}
-			try {
-				if (!command.autocomplete) return;
-				command.autocomplete(interaction);
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	},
+  name: "interactionCreate",
+  execute: (interaction: Interaction) => {
+    if (interaction.isChatInputCommand()) {
+      let command = interaction.client.slashCommands.get(
+        interaction.commandName
+      );
+      let cooldown = interaction.client.cooldowns.get(
+        `${interaction.commandName}-${interaction.user.username}`
+      );
+      if (!command) return;
+      if (command.cooldown && cooldown) {
+        if (Date.now() < cooldown) {
+          interaction.reply(
+            `You have to wait ${Math.floor(
+              Math.abs(Date.now() - cooldown) / 1000
+            )} second(s) to use this command again.`
+          );
+          setTimeout(() => interaction.deleteReply(), 5000);
+          return;
+        }
+        interaction.client.cooldowns.set(
+          `${interaction.commandName}-${interaction.user.username}`,
+          Date.now() + command.cooldown * 1000
+        );
+        setTimeout(() => {
+          interaction.client.cooldowns.delete(
+            `${interaction.commandName}-${interaction.user.username}`
+          );
+        }, command.cooldown * 1000);
+      } else if (command.cooldown && !cooldown) {
+        interaction.client.cooldowns.set(
+          `${interaction.commandName}-${interaction.user.username}`,
+          Date.now() + command.cooldown * 1000
+        );
+      }
+      command.execute(interaction);
+    } else if (interaction.isAutocomplete()) {
+      const command = interaction.client.slashCommands.get(
+        interaction.commandName
+      );
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+      try {
+        if (!command.autocomplete) return;
+        command.autocomplete(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
 };
 
 export default event;
--- apps/bot/src/handlers/Event.ts
+++ apps/bot/src/handlers/Event.ts
@@ -3,16 +3,16 @@ import { readdirSync } from "fs";
 import { join } from "path";
 import { BotEvent } from "../types";
 
-// This function read every file in the events folder and register them to the Discord Client
+// This code loads event files from a directory, creates a new event and registers it with the Discord client. The event can be executed once or on every occurrence, and the function logs the name of the event when successfully loaded.
 module.exports = (client: Client) => {
-	let eventsDir = join(__dirname, "../events");
+  let eventsDir = join(__dirname, "../events");
 
-	readdirSync(eventsDir).forEach((file) => {
-		if (!file.endsWith(".ts")) return;
-		let event: BotEvent = require(`${eventsDir}/${file}`).default;
-		event.once
-			? client.once(event.name, (...args) => event.execute(...args))
-			: client.on(event.name, (...args) => event.execute(...args));
-		console.log(`🌠 Successfully loaded event ${event.name}`);
-	});
+  readdirSync(eventsDir).forEach((file) => {
+    if (!file.endsWith(".ts")) return;
+    let event: BotEvent = require(`${eventsDir}/${file}`).default;
+    event.once
+      ? client.once(event.name, (...args) => event.execute(...args))
+      : client.on(event.name, (...args) => event.execute(...args));
+    console.log(`🌠 Successfully loaded event ${event.name}`);
+  });
 };
