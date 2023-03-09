dfca0c64c474fd3c0fadd406e339dad40ed0adda
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-09 11:23:17 -0500

    refactor: clean code

--- apps/main/next.config.mjs
+++ apps/main/next.config.mjs
@@ -4,7 +4,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -17,8 +17,8 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
 export default config;
