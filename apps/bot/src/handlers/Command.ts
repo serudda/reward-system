fe429a773dae978f920b5c18d9c0168294a0a44c
Zyruks <kuzenshi@gmail.com>
2023-03-09 02:01:01 -0400

    refactor: :recycle: simplify code and remove unused file
    Simplify `command.ts` file by removing unnecessary code related to registering commands in Discord REST API. Also remove `greet.ts` file, which is no longer used.
    
    The functionality related to registering commands in Discord REST API has been simplified.

--- apps/bot/src/handlers/Command.ts
+++ apps/bot/src/handlers/Command.ts
@@ -1,43 +1,33 @@
 import { Client, REST, Routes, SlashCommandBuilder } from "discord.js";
 import { readdirSync } from "fs";
 import { join } from "path";
-import { Command, SlashCommand } from "../types";
+import { SlashCommand } from "../types";
 
 // This function read every file in the slashCommands folder, create an slashCommand array and register them to the Discord REST API
 module.exports = (client: Client) => {
-	const slashCommands: SlashCommandBuilder[] = [];
-	const commands: Command[] = [];
+  const slashCommands: SlashCommandBuilder[] = [];
 
-	let slashCommandsDir = join(__dirname, "../slashCommands");
-	let commandsDir = join(__dirname, "../commands");
+  let slashCommandsDir = join(__dirname, "../slashCommands");
 
-	readdirSync(slashCommandsDir).forEach((file) => {
-		if (!file.endsWith(".ts")) return;
-		let command: SlashCommand = require(`${slashCommandsDir}/${file}`).default;
-		slashCommands.push(command.command);
-		client.slashCommands.set(command.command.name, command);
-	});
+  readdirSync(slashCommandsDir).forEach((file) => {
+    if (!file.endsWith(".ts")) return;
+    let command: SlashCommand = require(`${slashCommandsDir}/${file}`).default;
+    slashCommands.push(command.command);
+    client.slashCommands.set(command.command.name, command);
+  });
 
-	readdirSync(commandsDir).forEach((file) => {
-		if (!file.endsWith(".ts")) return;
-		let command: Command = require(`${commandsDir}/${file}`).default;
-		commands.push(command);
-		client.commands.set(command.name, command);
-	});
+  const rest = new REST({ version: "10" }).setToken(
+    process.env.DISCORD_BOT_TOKEN
+  );
 
-	const rest = new REST({ version: "10" }).setToken(
-		process.env.DISCORD_BOT_TOKEN
-	);
-
-	rest
-		.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
-			body: slashCommands.map((command) => command.toJSON()),
-		})
-		.then((data: any) => {
-			console.log(`Successfully loaded ${data.length} slash command(s)`);
-			console.log(`Successfully loaded ${commands.length} slash command(s)`);
-		})
-		.catch((e) => {
-			console.log(e);
-		});
+  rest
+    .put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
+      body: slashCommands.map((command) => command.toJSON()),
+    })
+    .then((data: any) => {
+      console.log(`Successfully loaded ${data.length} slash command(s)`);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,22 +1,21 @@
 import { Client, GatewayIntentBits, Collection } from "discord.js";
 const { Guilds, MessageContent, GuildMessages, GuildMembers } =
-	GatewayIntentBits;
-import { Command, SlashCommand } from "./types";
+  GatewayIntentBits;
+import { SlashCommand } from "./types";
 import { readdirSync } from "fs";
 import { join } from "path";
 
 // Initialize the Discord Client
 const client = new Client({
-	intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
+  intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
 });
 
 client.slashCommands = new Collection<string, SlashCommand>();
-client.commands = new Collection<string, Command>();
 client.cooldowns = new Collection<string, number>();
 
 const handlersDir = join(__dirname, "./handlers");
 readdirSync(handlersDir).forEach((handler) => {
-	require(`${handlersDir}/${handler}`)(client);
+  require(`${handlersDir}/${handler}`)(client);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
