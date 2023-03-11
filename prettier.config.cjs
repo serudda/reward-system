5c37b907d59419190a3b0e9d4868a0a61ac7f52e
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-11 18:40:10 -0500

    chore: include prettier prisma plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: [require.resolve('prettier-plugin-tailwindcss'), require.resolve('prettier-plugin-prisma')],
   semi: true,
   arrowParens: 'always',
   trailingComma: 'all',
