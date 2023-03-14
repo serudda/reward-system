c464b74d16eeb336294eee4a8b573fe631d226aa
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-13 23:02:13 -0500

    refactor: clean code

--- apps/bot/src/slashCommands/giveCoins.ts
+++ apps/bot/src/slashCommands/giveCoins.ts
@@ -6,23 +6,29 @@ import { type SlashCommand } from '../types';
 
 const showSentCoinsMsg = (interaction: CommandInteraction<CacheType>, coins: string) => {
   const receiver = interaction.options.getUser('user');
-  void interaction.reply(`${interaction.user} a agregado ${coins} Indie Tokens a wallet de ${receiver}.`);
+  void interaction.reply(`${interaction.user} has added ${coins} Indie Tokens to ${receiver}'s wallet.`);
 };
 
 /** Main command */
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName('give-coins')
-    .setDescription('Dar Indie Tokens a un usuario en específico')
-    .addUserOption((option) => option.setName('user').setDescription(`The receiver's username`).setRequired(true))
+    .setDescription(translate.commands.giveCoins.give)
+    .addUserOption((option) =>
+      option.setName('user').setDescription(translate.commands.giveCoins.receiver).setRequired(true),
+    )
     .addStringOption((option) =>
-      option.setName('coins').setDescription('Set the amount of coins you want to give.').setRequired(true),
+      option.setName('coins').setDescription(translate.commands.giveCoins.amount).setRequired(true),
     ),
   execute: async (interaction) => {
     const user = interaction.options.getUser('user');
     const coins: string = (interaction.options as any).getString('coins');
 
-    const updatedUser = await api.user.sendCoinsByUserId.mutate({ user: user as UserDiscord, coins: parseInt(coins) });
+    // Update or Create User
+    const updatedUser = await api.user.sendCoinsByUserId.mutate({
+      user: user as UserDiscord,
+      coins: parseInt(coins),
+    });
 
     if (updatedUser.data) showSentCoinsMsg(interaction, coins);
   },
