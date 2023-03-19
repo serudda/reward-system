b998517c0f2d1b6862e4c4982dcd962701d72f7f
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-19 18:18:43 -0500

    test: github actions

--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -28,7 +28,7 @@ export const botRouter = createTRPCRouter({
           });
         }
 
-        fetch(webhookUrl, {
+        await fetch(webhookUrl, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(data),
