81acbd684e4cc42d34f60cb12509d0e00b5aeddd
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-19 15:44:41 -0500

    refactor: include a nuevo router named "bot". With a first endpoint named "sendDiscordMsg"

---
+++ packages/api/src/router/bot.ts
@@ -0,0 +1,52 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
+
+export const botRouter = createTRPCRouter({
+  sendDiscordMsg: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        prUrl: z.string(),
+        coins: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      try {
+        console.log('input TEST ENTRO: ', input);
+        const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+        const data = {
+          username: 'El Banco',
+          content: `El pull request #${input.prUrl} ha sido mergeado en Develop. Se otorgan ${input.coins} puntos a ${input.username}.`,
+        };
+
+        if (!webhookUrl) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: 'Webhook URL not found',
+          });
+        }
+
+        fetch(webhookUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data),
+        })
+          .then(() => console.log('Mensaje enviado correctamente.'))
+          .catch((error) => console.error('Error al enviar mensaje:', error));
+        // if (!user) {
+        //   throw new TRPCError({
+        //     code: 'NOT_FOUND',
+        //     message: 'User with that ID not found',
+        //   });
+        // }
+      } catch (err: any) {
+        throw err;
+      }
+    }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return 'you can now see this secret message!';
+  }),
+});
