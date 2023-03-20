260657deddffdffa1f005a31aaddf43d0d7a9276
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-20 08:55:04 -0500

    test: github actions

--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -19,9 +19,12 @@ export const botRouter = createTRPCRouter({
         const data = {
           username: 'Reward System',
           content: `
-            **${input.username}** has been rewarded with **${input.coins}** Indie Tokens 💎.
-            → For merging the following pull request in Develop:
-            ${input.prUrl}
+          :discordseruddaemoji02: :mega: 
+          ---------------
+          **${input.username}** has been rewarded with **${input.coins}** Indie Tokens :gem:.
+          → For merging the following pull request in Develop:
+          ${input.prUrl}
+          ---------------
           `,
         };
 
