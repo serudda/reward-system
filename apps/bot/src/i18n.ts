bb85d38697075479a5b05eef55ab0625c3289af1
Santiago Rodriguez <05rmsantiago@gmail.com>
2023-03-27 18:33:18 -0300

    feat: fix problems whit i18n prettier format

--- apps/bot/src/i18n.ts
+++ apps/bot/src/i18n.ts
@@ -1,13 +1,10 @@
-/* Dependencies */
-import { readdirSync } from 'fs';
+import fs, { readdirSync } from 'fs';
 import path from 'path';
 import i18n from 'i18next';
 import LenguajeDetection from 'i18next-browser-languagedetector';
 import i18nextMiddleware from 'i18next-http-middleware';
 
-/**
- * This interface, define the generic type of the json files.
- */
+//This interface, define the generic type of the json files.
 interface JsonInterface<T> {
   [key: string]: T;
 }
@@ -20,11 +17,12 @@ interface JsonInterface<T> {
 const loadLocaleFiles = () => {
   const localesDir = path.join(__dirname, 'i18n');
   const locales = readdirSync(localesDir);
-  const resources: {[key: string]: JsonInterface<string>} = {};
+  const resources: { [key: string]: JsonInterface<string> } = {};
   for (const locale of locales) {
     const filepath = path.join(localesDir, locale);
-    const fileContent: string = <string>require(filepath);
-    resources[locale.replace('.json', '')] = { translation: fileContent  };
+    const fileContent: string = fs.readFileSync(filepath, 'utf-8');
+    const tempResource = { translation: fileContent };
+    resources[locale.replace('.json', '')] = tempResource;
   }
   return resources;
 };
@@ -35,23 +33,21 @@ const loadLocaleFiles = () => {
  * and the detection user languages.
  * @returns i18n object
  */
-    
-    i18n
-    .use(i18nextMiddleware.LanguageDetector)
-    .use(LenguajeDetection)
-    .init({
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false, // This line, pemit the injection of discord markdown in the translations.
-      },
-      detection: {
-        order: ['querystring', 'cookie', 'header'],
-        caches: ['cookie'],
-        lookupCookie: 'lenguaje',
-      },
-      resources: loadLocaleFiles(),
-    });
 
-  
+void i18n
+  .use(i18nextMiddleware.LanguageDetector)
+  .use(LenguajeDetection)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // This line, pemit the injection of discord markdown in the translations.
+    },
+    detection: {
+      order: ['querystring', 'cookie', 'header'],
+      caches: ['cookie'],
+      lookupCookie: 'lenguaje',
+    },
+    resources: loadLocaleFiles(),
+  });
 
 export default i18n;
