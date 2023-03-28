d4cec9695ee6fb34142421c7c651b7f3a6e145cc
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-03-27 21:04:08 -0500

    refactor: first version of the Store connected with the DB

--- packages/api/src/router/store.ts
+++ packages/api/src/router/store.ts
@@ -4,4 +4,12 @@ export const storeRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.store.findMany();
   }),
+
+  getAllWithItems: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.store.findMany({
+      include: {
+        items: true,
+      },
+    });
+  }),
 });
