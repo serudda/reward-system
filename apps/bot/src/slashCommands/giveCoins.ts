200c3099fd0d103c7b50f1eba2d344b31cd5aa7e
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-04-01 04:43:06 -0500

    refactor: include a try catch section

--- apps/bot/src/slashCommands/giveCoins.ts
+++ apps/bot/src/slashCommands/giveCoins.ts
@@ -6,9 +6,9 @@ import { type SlashCommand } from '../types';
 const showSentCoinsMsg = (interaction: CommandInteraction<CacheType>, coins: string) => {
   const receiver = interaction.options.getUser('user');
   const message = i18n.t('app.bot.command.giveCoins.success.give', {
-    sender: interaction.user,
+    sender: `<@${interaction.user.id}>`,
     coins,
-    receiver,
+    receiver: `<@${receiver?.id}>`,
   });
 
   void interaction.reply(message);
@@ -26,17 +26,24 @@ const command: SlashCommand = {
       option.setName('coins').setDescription(i18n.t('app.bot.command.giveCoins.amount')).setRequired(true),
     ),
   execute: async (interaction) => {
-    const user = interaction.options.getUser('user');
-    // TODO: Fix an Type issue with .getString, it is not recognized as a function
-    const coins: string = (interaction.options as any).getString('coins'); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    try {
+      const user = interaction.options.getUser('user');
+      // TODO: Fix an Type issue with .getString, it is not recognized as a function
+      const coins: string = (interaction.options as any).getString('coins'); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
 
-    // Update or Create User
-    const updatedUser = await api.user.sendCoinsByUserId.mutate({
-      user: user as UserDiscord,
-      coins: parseInt(coins),
-    });
+      // Update or Create User
+      const response = await api.user.sendCoinsByUserId.mutate({
+        user: user as UserDiscord,
+        coins: parseInt(coins),
+      });
 
-    if (updatedUser.data) showSentCoinsMsg(interaction, coins);
+      if (response?.data) showSentCoinsMsg(interaction, coins);
+    } catch (error: any) {
+      if (!error) return;
+      await interaction.reply({
+        content: error?.message ? error.message : i18n.t('common.message.error.internalError'), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+      });
+    }
   },
   cooldown: 10,
 };
