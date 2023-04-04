de073b4807be356655a27385c4eb7ba5a866ba22
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-04-04 00:06:32 -0500

    refactor: improve the management of the env variables

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -1,14 +1,12 @@
 import { httpBatchLink, loggerLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import superjson from 'superjson';
-
 import type { AppRouter } from '@acme/api';
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:3000`; // dev SSR should use localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
 export const api = createTRPCNext<AppRouter>({
