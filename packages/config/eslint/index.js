420e9e38cf528cb331a30806abd4bcaeec332b61
Zyruks <kuzenshi@gmail.com>
2023-04-07 20:51:26 -0300

    build: :bricks: update starter template and dependecies
    typescript major update 4.9.5 to 5.0.3,

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -16,6 +16,7 @@ const config = {
     '@typescript-eslint/no-unsafe-call': 'warn',
     '@typescript-eslint/no-misused-promises': 'warn',
     '@typescript-eslint/no-unsafe-argument': 'warn',
+    "@typescript-eslint/ban-ts-comment": "warn",
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
