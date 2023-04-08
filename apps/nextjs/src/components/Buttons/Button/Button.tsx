ac9c1ba940ac6dd09c5295d30be53fb687657584
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-04-07 19:46:14 -0500

    refactor: refactor Button component in order to include project style

--- apps/nextjs/src/components/Buttons/Button/Button.tsx
+++ apps/nextjs/src/components/Buttons/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React, { type ButtonHTMLAttributes, type ReactNode } from 'react';
 import cn from 'classnames';
-import { Icon, type Catalog as IconCatalog } from '~/components';
+import { Icon, type IconCatalog } from '~/components';
 
 export enum ButtonSize {
   xs = 'xs',
@@ -9,27 +9,27 @@ export enum ButtonSize {
 }
 
 const SizesWithoutIcon: Record<ButtonSize, string> = {
-  [ButtonSize.xs]: 'py-2 px-4 text-sm font-medium',
-  [ButtonSize.sm]: 'py-2 px-4 text-base font-medium',
-  [ButtonSize.base]: 'p-4 text-base font-medium',
+  [ButtonSize.xs]: 'py-2 px-4 text-xs font-semibold',
+  [ButtonSize.sm]: 'py-2 px-4 text-sm font-semibold',
+  [ButtonSize.base]: 'py-3 px-5 text-base font-medium',
 };
 
 const SizesOnlyIcon: Record<ButtonSize, string> = {
-  [ButtonSize.xs]: 'p-1 text-sm font-medium',
-  [ButtonSize.sm]: 'p-2 text-base font-medium',
-  [ButtonSize.base]: 'p-4 text-base font-medium',
+  [ButtonSize.xs]: 'p-2',
+  [ButtonSize.sm]: 'p-2',
+  [ButtonSize.base]: 'p-3',
 };
 
 const SizesWithStartIcon: Record<ButtonSize, string> = {
-  [ButtonSize.xs]: 'pl-1 pr-2 py-1 text-sm font-medium',
-  [ButtonSize.sm]: 'pl-2 pr-4 py-2 text-base font-medium',
-  [ButtonSize.base]: 'pl-2 pr-4 py-4 text-base font-medium',
+  [ButtonSize.xs]: 'px-3 py-2 text-xs font-semibold',
+  [ButtonSize.sm]: 'px-4 py-2 text-sm font-semibold',
+  [ButtonSize.base]: 'px-4 py-3 text-base font-semibold',
 };
 
 const SizesWithEndIcon: Record<ButtonSize, string> = {
-  [ButtonSize.xs]: 'pl-2 pr-1 py-1 text-sm font-medium',
-  [ButtonSize.sm]: 'pl-4 pr-2 py-2 text-base font-medium',
-  [ButtonSize.base]: 'pl-4 pr-2 py-4 text-base font-medium',
+  [ButtonSize.xs]: 'px-3 py-2 text-xs font-semibold',
+  [ButtonSize.sm]: 'px-4 py-2 text-sm font-semibold',
+  [ButtonSize.base]: 'px-4 py-3 text-base font-semibold',
 };
 
 export enum ButtonVariant {
@@ -57,9 +57,9 @@ const DisabledVariants: Record<ButtonVariant, string> = {
 };
 
 enum ButtonIconSize {
-  xs = '24',
-  sm = '24',
-  base = '24',
+  xs = 'w-4 h-4',
+  sm = 'w-5 h-5',
+  base = 'w-6 h-6',
 }
 
 export enum HtmlType {
@@ -155,11 +155,11 @@ export const Button = ({
         'cursor-default opacity-30': isDisabled,
       },
     ),
-    startIcon: cn({
+    startIcon: cn(ButtonIconSize[size], {
       'mr-1': children && size === ButtonSize.xs,
       'mr-2': children && (size === ButtonSize.sm || size === ButtonSize.base),
     }),
-    endIcon: cn({
+    endIcon: cn(ButtonIconSize[size], {
       'ml-1': children && size === ButtonSize.xs,
       'ml-2': children && (size === ButtonSize.sm || size === ButtonSize.base),
     }),
@@ -197,9 +197,7 @@ export const Button = ({
       )}
       <span>{children}</span>
       {isLoading && <span className={classes.loading}></span>}
-      {endIcon && (
-        <Icon className={classes.endIcon} icon={endIcon} width={ButtonIconSize[size]} height={ButtonIconSize[size]} />
-      )}
+      {endIcon && <Icon className={classes.endIcon} icon={endIcon} />}
     </button>
   );
 };
---
+++ apps/nextjs/src/pages/setting/index.tsx
@@ -0,0 +1,37 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import { api } from '~/utils/api';
+import { ItemsGrid } from '~/components/ItemsGrid/ItemsGrid';
+import { Navbar } from '~/components/Navbar/Navbar';
+
+const Setting: NextPage = () => {
+  const { data: stores, isLoading, isError } = api.store.getAllWithItems.useQuery();
+
+  const renderStores = () => {
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error</div>;
+
+    return stores.map((store) => (
+      <ItemsGrid key={store.id} storeName={store.name} storeImageUrl={store.imageUrl} items={store.items} />
+    ));
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Reward System - Store </title>
+        <meta name="description" content="Reward System - Store" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="relative mx-auto min-h-screen max-w-screen-xl px-6 pb-20 sm:px-8">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Main Content */}
+        <div className="pt-20">{renderStores()}</div>
+      </div>
+    </>
+  );
+};
+
+export default Setting;
