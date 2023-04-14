b40dfb5204f07cb514a99e108f7fbedf42442b00
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-04-14 15:58:06 -0500

    refactor: clean code

--- packages/config/i18n/index.ts
+++ packages/config/i18n/index.ts
@@ -1,32 +1,7 @@
-// import { readFileSync, readdirSync } from 'fs';
-// import path from 'path';
 import i18n from 'i18next';
 import LenguajeDetection from 'i18next-browser-languagedetector';
 import i18nextMiddleware from 'i18next-http-middleware';
 import resourcesToBackend from 'i18next-resources-to-backend';
-
-//This interface, define the generic type of the json files.
-interface JsonInterface<T> {
-  [key: string]: T;
-}
-
-/**
- * This functions search translation files json,
- * and return the content of files.
- * @returns json content
- */
-// const loadLocaleFiles = () => {
-//   const localesDir = path.join(__dirname, 'locale');
-//   const locales = readdirSync(localesDir);
-//   const resources: { [key: string]: JsonInterface<string> } = {};
-//   for (const locale of locales) {
-//     const filepath = path.join(localesDir, locale);
-//     const fileContent: string = readFileSync(filepath, 'utf-8');
-//     const tempResource = { translation: JSON.parse(fileContent) as string };
-//     resources[locale.replace('.json', '')] = tempResource;
-//   }
-//   return resources;
-// };
 
 /**
  * i18n Configuration
@@ -66,7 +41,6 @@ void i18n
       lookupCookie: 'lenguaje',
     },
     debug: false,
-    // resources: loadLocaleFiles(),
   });
 
 export default i18n;
