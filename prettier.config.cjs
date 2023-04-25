f3996d83f2b20ed92122dfd98fa3db66a894c187
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-04-24 19:48:04 -0500

    refactor: clean code

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -17,7 +17,6 @@ const config = {
   importOrder: [
     '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
     '^(next/(.*)$)|^(next$)',
-    '^(expo(.*)$)|^(expo$)',
     '<THIRD_PARTY_MODULES>',
     '^@acme/(.*)$',
     '^~/utils/(.*)$',
